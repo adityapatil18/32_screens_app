bec1d3a79669162bea4e4b26cb2fd3cb017eb0d7
adityapatil18 <adityagangane2018@gmail.com>
2023-10-09 16:43:39 +0530

    updated

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:screens_practice/login_screen2.dart';
-import 'package:screens_practice/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
